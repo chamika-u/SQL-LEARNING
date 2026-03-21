cd0d929377d2d37deeff7a1c668711b338387e8d
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-03-21 23:16:49 +0530

    Add UNION example to demonstrate combining queries in advanced data querying SQL file

--- 3_ADVANCED_DATA_QUERYING.sql
+++ 3_ADVANCED_DATA_QUERYING.sql
@@ -14,3 +14,11 @@ SELECT customer_name, total_quantity FROM customer c FULL OUTER JOIN orders o ON
 
 SELECT customer_name, name, total_quantity FROM customer c FULL OUTER JOIN orders o ON c.customer_id = o.customer_id FULL OUTER JOIN
 products p ON p.product_id = o.product_id;
+
+-- UNION (Combining 2 Queries at once)
+select name from products where category_id =1
+
+UNION
+
+select name from products p inner join orders o on o.product_id = p.product_id where o.total_quantity > 2
+
