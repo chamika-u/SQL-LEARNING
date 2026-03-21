40c889f681187a5f32bd29743d5fce19eb308b2f
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-03-21 22:49:48 +0530

    Fix SQL syntax by adding missing semicolons and correcting JOIN statements in advanced data querying file

--- 3_ADVANCED_DATA_QUERYING.sql
+++ 3_ADVANCED_DATA_QUERYING.sql
@@ -3,11 +3,14 @@ SELECT name, description, total_amount FROM Orders o INNER JOIN Products p ON p.
 SELECT customer_name, total_amount FROM orders o JOIN customer c ON c.customer_id = o.customer_id; -- names of the custormers who purchased products and their total amount of the order.
 
 -- LEFT JOIN
-SELECT customer_name, total_amount FROM customer c LEFT JOIN orders o ON c.customer_id = o.customer_id
-SELECT name, total_quantity FROM orders o LEFT JOIN products p ON p.product_id = o.product_id
+SELECT customer_name, total_amount FROM customer c LEFT JOIN orders o ON c.customer_id = o.customer_id;
+SELECT name, total_quantity FROM orders o LEFT JOIN products p ON p.product_id = o.product_id;
 
 -- RIGHT JOIN
-SELECT name, total_quantity FROM orders o RIGHT JOIN products p ON p.product_id = o.product_id
+SELECT name, total_quantity FROM orders o RIGHT JOIN products p ON p.product_id = o.product_id;
 
 -- FULL OUTER
-SELECT name, description, total_amount FROM Orders o FULL OUTER JOIN Products p ON p.product_id=o.product_id;
+SELECT customer_name, total_quantity FROM customer c FULL OUTER JOIN orders o ON c.customer_id = o.customer_id;
+
+SELECT customer_name, name, total_quantity FROM customer c FULL OUTER JOIN orders o ON c.customer_id = o.customer_id FULL OUTER JOIN
+products p ON p.product_id = o.product_id;
