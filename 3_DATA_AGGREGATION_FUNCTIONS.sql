1616ec9fe1f63617c2135b70a82caf92ad17540a
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-03-25 17:50:10 +0530

    Add aggregation functions for MAX, MIN, AVG, and Standard Deviation in SQL file

--- 3_DATA_AGGREGATION_FUNCTIONS.sql
+++ 3_DATA_AGGREGATION_FUNCTIONS.sql
@@ -7,4 +7,15 @@ SELECT COUNT(DISTINCT customer_id) AS customer_count FROM customer; -- DISTINCT 
 SELECT SUM(total_amount) AS sum_of_total FROM orders; 
 SELECT SUM(DISTINCT total_amount) AS sum_of_total FROM orders; 
 
+--MAX
+SELECT MAX(total_amount) AS max_amount FROM orders;
+
+--MIN
+SELECT MIN(total_amount) AS min_amount FROM orders;
+
+--AVG
+SELECT AVG(total_amount) AS avg_amount FROM orders;
+
+--Standard Deviation
+SELECT STDDEV(total_amount) AS std_amount FROM orders;
 
