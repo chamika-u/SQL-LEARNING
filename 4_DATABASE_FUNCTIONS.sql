e15a4017525a4e8386a9d07f111304faecb547fd
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-02 11:26:31 +0530

    Add SQL examples for ABS, CEIL, FLOOR, ROUND, and SQRT functions

---
+++ 4_DATABASE_FUNCTIONS.sql
@@ -0,0 +1,5 @@
+SELECT ABS(2.6); --abs value
+SELECT CEIL(2.3); --Highest integer for a decimal number
+SELECT FLOOR(2.3); --Lowest integer for a decimal number
+SELECT ROUND(2.3456, 2); --(Number, decimal points wants to round)
+SELECT SQRT(144); --Find square root of a number
