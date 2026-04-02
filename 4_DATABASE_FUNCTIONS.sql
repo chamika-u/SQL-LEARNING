aeeafa8fac59fad46c26ef0f035b7bb4eaf9d23e
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-02 11:38:14 +0530

    Add examples for date functions in SQL

--- 4_DATABASE_FUNCTIONS.sql
+++ 4_DATABASE_FUNCTIONS.sql
@@ -1,3 +1,5 @@
+--Mathematical Functions
+
 SELECT ABS(2.6); --abs value
 SELECT CEIL(2.3); --Highest integer for a decimal number
 SELECT FLOOR(2.3); --Lowest integer for a decimal number
@@ -5,3 +7,11 @@ SELECT ROUND(2.3456, 2); --(Number, decimal points wants to round)
 SELECT SQRT(144); --Find square root of a number
 SELECT SQRT(4.0);
 SELECT ROUND(SQRT(4.0),0);-- Find square root of a given number and round it to 2 decimal points.
+
+--Date Functions
+
+SELECT CURRENT_DATE; -- Seleclt current date
+SELECT EXTRACT(DAY FROM CURRENT_DATE); --Select only the day from current date
+SELECT EXTRACT(MONTH FROM CURRENT_DATE); --Select only the month from current date
+SELECT EXTRACT(YEAR FROM CURRENT_DATE); --Select only the year from current date
+
