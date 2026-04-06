eaf284e5789e628ff83f7955985e7371af91e60c
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-07 04:16:57 +0530

    Fix formatting of analytical function comments and remove extra spaces in SQL examples

--- 5_ANALYTICAL_FUNCTIONS.sql
+++ 5_ANALYTICAL_FUNCTIONS.sql
@@ -1,13 +1,18 @@
 -- RANK() FUNCTION
 SELECT customer_id,
-RANK () OVER (PARTITION BY customer_id ORDER BY total_amount DESC) AS order_rank, total_amount FROM Orders;
+RANK() OVER (PARTITION BY customer_id ORDER BY total_amount DESC) AS order_rank, total_amount FROM Orders;
 /*IN HERE SUB SEQUENT RANK WILL SKIPPED*/
 
---DENSE_RANK () FUNCTION
+--DENSE_RANK() FUNCTION
 SELECT customer_id,
-DENSE_RANK () OVER (PARTITION BY customer_id ORDER BY total_amount DESC) AS order_rank, total_amount FROM Orders;
+DENSE_RANK() OVER (PARTITION BY customer_id ORDER BY total_amount DESC) AS order_rank, total_amount FROM Orders;
 /*IN HERE SUB SEQUENT RANK WILL NOT SKIPPED*/
 
--- ROW_NUMBER () FUNCTION
+-- ROW_NUMBER() FUNCTION
 SELECT order_id, customer_id, order_timestamp,
 ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_timestamp ASC) AS order_seq_num FROM Orders;
+
+--LAG() Function
+SELECT customer_id, order_id, order_timestamp,
+LAG(order_timestamp, 1) OVER (PARTITION BY customer_id ORDER BY order_timestamp ASC) AS previous_order FROM Orders;
+
