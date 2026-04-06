c70ea9651e3bd00b44da7c6135ce6968ac0ff16b
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-07 03:48:43 +0530

    Add example for ROW_NUMBER function in SQL

--- 5_ANALYTICAL_FUNCTIONS.sql
+++ 5_ANALYTICAL_FUNCTIONS.sql
@@ -8,3 +8,6 @@ SELECT customer_id,
 DENSE_RANK () OVER (PARTITION BY customer_id ORDER BY total_amount DESC) AS order_rank, total_amount FROM Orders;
 /*IN HERE SUB SEQUENT RANK WILL NOT SKIPPED*/
 
+-- ROW_NUMBER () FUNCTION
+SELECT order_id, customer_id, order_timestamp,
+ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_timestamp ASC) AS order_seq_num FROM Orders;
