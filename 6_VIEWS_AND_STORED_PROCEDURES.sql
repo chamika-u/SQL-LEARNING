13724e5b97fe178516c5157760f0ad0dd916502e
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-08 01:50:29 +0530

    Add SQL procedure for adding categories and update comments for clarity

--- 6_VIEWS_AND_STORED_PROCEDURES.sql
+++ 6_VIEWS_AND_STORED_PROCEDURES.sql
@@ -10,3 +10,18 @@ FROM Orders GROUP BY customer_id;
 SELECT * FROM customer_order_summery;
 
 /*Using view hide complexity of the query and simplifies for people who can't handle large queries in SQL*/
+
+--Creating Procedures
+CREATE PROCEDURE add_category(cat_id int, cat_name varchar)
+LANGUAGE plpgsql
+AS
+$$
+BEGIN
+	INSERT INTO categories VALUES(cat_id, cat_name);
+END;
+$$;
+
+CALL add_category(6, 'Fashion');
+SELECT * FROM categories;
+
+/*These procedures works like a functions in object oriented programming languages such as Java*/
