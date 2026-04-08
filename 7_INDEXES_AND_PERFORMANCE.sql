e6d35e332c7ebb383e590479ce6bbb52d1fdf2c9
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-09 03:57:22 +0530

    Enhance index usage checks and performance analysis in orders table

--- 7_INDEXES_AND_PERFORMANCE.sql
+++ 7_INDEXES_AND_PERFORMANCE.sql
@@ -8,5 +8,14 @@ CREATE INDEX idx_cust_order ON orders(customer_id, order_timestamp);
 -- Using EXPLAIN ANALYZE to see the performance of a query with and without indexes
 EXPLAIN ANALYZE SELECT * FROM orders WHERE order_id=1;
 
--- Check index usage
-SELECT * FROM pg_stat_user_indexes WHERE relname='orders';
+SELECT * FROM pg_stat_user_indexes WHERE relname='orders'; -- Check index usage statistics for the 'orders' table
+
+EXPLAIN SELECT * FROM orders WHERE order_id=1; -- Check the query plan to see if the index is being used
+
+-- Basic performance tunning
+
+SELECT * FROM orders WHERE customer_id=1 order by order_timestamp;
+-- This is not a a very optimized query
+
+CREATE INDEX idx_cust ON orders(customer_id, order_timestamp);
+-- After this index was created above query works faster.
