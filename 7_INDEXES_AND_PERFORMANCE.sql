c16bdb0e707ff0f579d21740895e3f678fdddcbd
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-09 03:39:04 +0530

    Add SQL settings for PostgreSQL connection configuration

--- 7_INDEXES_AND_PERFORMANCE.sql
+++ 7_INDEXES_AND_PERFORMANCE.sql
@@ -4,3 +4,7 @@ CREATE INDEX idx_customer_email ON customer(email);
 CREATE INDEX idx_prod_cat ON products(category_id);
 CREATE UNIQUE INDEX idx_prod_name ON products(name);
 CREATE INDEX idx_cust_order ON orders(customer_id, order_timestamp);
+
+-- Using EXPLAIN ANALYZE to see the performance of a query with and without indexes
+EXPLAIN ANALYZE SELECT * FROM orders WHERE order_id=1;
+
