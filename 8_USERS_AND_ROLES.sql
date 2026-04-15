41346580aa7f3e3fe642fff2c609226e023209de
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-16 03:54:29 +0530

    Fix password for user role chamika4 and add chamika5 with inherited access

--- 8_USERS_AND_ROLES.sql
+++ 8_USERS_AND_ROLES.sql
@@ -13,5 +13,6 @@ CREATE ROLE chamika2 WITH login PASSWORD 'chamika-u' createrole; -- Giving acces
 
 CREATE ROLE chamika3 WITH login PASSWORD 'chamika-u' VALID UNTIL '2026-09-29'; -- Giving access to the user until a specific date
 
-CREATE ROLE chamika4 WITH login PASSWORD 'chamika0+-u' CONNECTION LIMIT 10; -- Giving access to the user with a limit of 10 connections
+CREATE ROLE chamika4 WITH login PASSWORD 'chamika-u' CONNECTION LIMIT 10; -- Giving access to the user with a limit of 10 connections
 
+CREATE ROLE chamika5 WITH login PASSWORD 'chamika-u' IN ROLE chamika4; -- Giving access to the user with the same access as chamika4
