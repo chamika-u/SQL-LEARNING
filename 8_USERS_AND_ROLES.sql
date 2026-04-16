453157edcd46d6a5b4fdf3e502ef0a3b1738a446
Chamika Udayanga <c.udayanga2k04@gmail.com>
2026-04-17 02:09:31 +0530

    Add grant statement for chamika1 to allow select permission on customer table

--- 8_USERS_AND_ROLES.sql
+++ 8_USERS_AND_ROLES.sql
@@ -1,10 +1,12 @@
--- creating an user
+-- CREATING AN USER
 CREATE ROLE mike WITH login;
 
--- creating an user with password
+
+-- CREATING USERS WITH PASSWORD
 CREATE ROLE tommy WITH login PASSWORD 'tom@123';
 
--- creating users and giving access to them
+
+-- CREATING USERS AND GIVING ACCESS TO THEM
 CREATE ROLE chamika WITH login PASSWORD 'chamika-u' superuser; -- Giving all the access
 
 CREATE ROLE chamika1 WITH login PASSWORD 'chamika-u' createdb; -- Giving access to create databases 
@@ -18,3 +20,21 @@ CREATE ROLE chamika4 WITH login PASSWORD 'chamika-u' CONNECTION LIMIT 10; -- Giv
 CREATE ROLE chamika5 WITH login PASSWORD 'chamika-u' IN ROLE chamika4; -- Giving access to the user with the same access as chamika4
 
 CREATE USER chamika6 WITH PASSWORD 'chamika-u'; -- creating an user with the same access as chamika4
+
+
+-- GRANTING AND REVOKING USER PERMISSIONS
+-- Granting
+GRANT SELECT ON customer TO chamika1; -- Granting select permission on customer table to chamika1
+GRANT DELETE, INSERT ON customer TO chamika1; -- Granting delete and insert permissions on customer table to chamika1
+
+GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO chamika1; -- Granting select permission on all tables in public schema to chamika1
+GRANT SELECT ON customer TO chamika1 WITH GRANT OPTION; -- Granting select permission on customer table to chamika1 with the ability to grant the same permission to others
+
+-- Revoking
+REVOKE SELECT ON customer FROM chamika1; -- Revoking select permission on customer table from chamika1
+REVOKE DELETE, INSERT ON customer FROM chamika1; -- Revoking delete and insert permissions on customer table from chamika1
+
+
+-- USING ROLES FOR PERMISSION MANAGEMENT
+CREATE ROLE sales_team; -- Creating a role for the sales team
+CREATE ROLE admins; -- Creating a role for the admins
